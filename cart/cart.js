faa1625ff47b2cf7087718a867aabc98b6cd9f19
Sabrina Sampaio <sassa.sampaio@gmail.com>
2021-08-14 19:09:01 -0300

    Fixed cart and login pages

---
+++ cart/cart.js
@@ -0,0 +1,100 @@
+getCartPage();
+renderItems();
+
+function getCartPage(){  
+    if(getInfomationCookie()[0].length==0){
+        fetch("cart.html")
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                document.querySelector("main").innerHTML = data;
+            })
+            .then(()=>{
+                document.querySelector("head").insertAdjacentHTML('afterbegin', '<link rel="stylesheet" href="cart.css"></link>');
+            });
+    }else{
+        fetch("cartItems.html")
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                document.querySelector("main").innerHTML = data;
+            })
+            .then(()=>{
+                document.querySelector("head").insertAdjacentHTML('afterbegin', '<link rel="stylesheet" href="cartItems.css"></link>');
+            });
+    }   
+}
+
+function getCartProductsBd(id, qtd) {
+    const token = 'd2e7727e16065b64a486255d82e999';
+    fetch(
+        'https://graphql.datocms.com/',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `${token}`,
+            },
+            body: JSON.stringify({
+                query: `
+                {
+                    allProducts(
+                      filter: {
+                        id: { eq: "${id}" }
+                      }
+                    ) {
+                      name 
+                      price
+                    }
+                }`
+            }),
+        }
+    )
+        .then(res => res.json())
+        .then((res) => {
+            if (res.data.allProducts.length !== 0) {
+                
+                renderProductsCart(res.data.allProducts[0], qtd);
+                
+            } else {
+                alert("Acesso Negado!!!!!!!");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+}
+
+function renderProductsCart(product, qtd) {
+    
+    console.log(product);
+    let listItems = document.getElementById("items-to-display");
+    if (!listItems)
+        return;
+    let htmlInsert = `
+        <div class="products">
+                <img src="../about/images/doces.png">
+                <h4>${product.name}</h4>
+                <h4>Quantidade: ${qtd}</h4>
+                <h4>R$${product.price},00</h4>
+                <i class="fa fa-times-circle fa-2x"></i>
+        </div>
+    `
+    listItems.insertAdjacentHTML('beforeend', htmlInsert)  
+}
+
+function renderItems(){
+    let cartProducts= getCookieIdQtd();
+   
+    cartProducts.map(infoProd=>{
+        getCartProductsBd(infoProd[0],infoProd[1]);
+    })
+
+}
+
+
+
